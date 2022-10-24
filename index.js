5657cad8198c729abb6c45be30cbe7ab8159a617
Amgad <amgadthabit9@gmail.com>
2022-10-24 06:48:03 +0300

    Score

---
+++ index.js
@@ -0,0 +1,34 @@
+let saveEl = document.getElementById("zero1")
+let saveXl = document.getElementById("zero")
+
+let count = 0
+let count1 = 0
+
+function but(){
+    count = count + 1
+    saveEl.textContent = count
+    
+}
+function but1(){
+    count += 2
+    saveEl.textContent = count
+    
+}
+function but2(){
+    count += 3
+    saveEl.textContent = count
+    
+}
+function button(){
+    count1 +=1 
+    saveXl.textContent = count1
+}
+function button1(){
+    count1 +=2
+    saveXl.textContent = count1    
+}
+function button2(){
+    count1 +=3
+    saveXl.textContent = count1   
+}
+console.log(count)
